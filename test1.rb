7026c52a379c5f7dd1f65b856d16f2905a19e79b
xuechong87 <xuechong87@gmail.com>
2013-11-19 00:14:14 +0800

    simple delegator

---
+++ test1.rb
@@ -0,0 +1,39 @@
+class SimpleDelegator
+	
+	Preserved = ["__id__","object_id","__send__","respond_to?"]
+	instance_methods.each do |m|
+	next if preserved.include?(m)
+	undef_method m
+	end
+
+
+	def initialize(obj)
+		@_sd_obj = obj
+	end
+
+
+
+	def method_missing(m,*args)
+		unless @_sd_obj.respond_to?(m)
+			super(m,*args)		
+		end
+		@_sd_obj.__send__(m,*args)
+	end
+
+
+	def respond_to?(m)
+		return true if super
+		return @_sd_obj.respond_to?(m)
+	end
+
+
+end
+
+class Test
+	def dosome()
+		puts "test"
+	end
+end
+
+d = SimpleDelegator.new(Test.new())
+d.dosome()
