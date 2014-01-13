51430cbc4f0222162919402be36b2344ad593ff8
xuechong <xuechong87@gmail.com>
2014-01-13 08:16:12 +0800

    add undef methods

---
+++ undef.rb
@@ -0,0 +1,31 @@
+
+begin
+	class C1 < Object
+		def m1
+			p "m1"
+		end
+	end
+
+	class C2 < C1
+		def m2
+			p "m2"
+		end
+	end
+
+	c = C2.new
+	c.m1
+	c.m2
+
+	class C2
+		undef m1
+		undef m2
+	end
+
+	c.m1 #NO method error :undefined method 
+	c.m2
+rescue Exception => e
+	p e
+end
+
+p "end"
+gets "end"
